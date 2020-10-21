3cf773bb165eca58153e43adb7924092dfd8355c
root <root@masterserver.progressive.my.id>
2020-10-22 05:30:41 +0700

    update iot-pinger

--- python/iotac-pinger.py
+++ python/iotac-pinger.py
@@ -2,8 +2,8 @@
 
 ###################################
 # Feature Update : 
-#   Nyimpan Output to MongoDB
 #   Get Data from mosquitto
+#   Set Historical Data
 ###################################
 from influxdb import InfluxDBClient
 from influxdb.client import InfluxDBClientError
@@ -21,8 +21,9 @@ NINPINGOK=0
 NINPINGNOK=0
 NOUTPINGOK=0
 NOUTPINGNOK=0
-PING_INTERVAL="10"
 ROWS = []
+
+PING_INTERVAL="10"
 LIST_DATA_AC_PATH = '/github/progressive/python/logs/LIST_DATA_AC.csv'
 LIST_DATA_AC_COMPLETE_PATH = '/DRIVE-C/Users/mochamad/OneDrive - PT Astra Honda Motor/Notebooks/SYNC2LINUX/LIST_DATA_AC.csv'
 OS = platform.system()
@@ -36,7 +37,7 @@ def initialization_influx():
 
     global SERIES
     try:
-        if OS == 'posix' :
+        if OS == 'Linux' :
             SERIES = InfluxDBClient(INFLUX_HOST, INFLUX_PORT, INFLUX_USERNAME, INFLUX_PASSWORD)
         else :
             # windows kalau beda -->
@@ -61,8 +62,8 @@ def initialization_mongo():
     
     global COLLECTION
     try:
-        if OS == 'posix' :
-            client = MongoClient("mongodb://192.168.0.100:27017/",serverSelectionTimeoutMS=maxSevSelDelay)
+        if OS == 'Linux' :
+            client = MongoClient("mongodb://192.168.56.1:27017/",serverSelectionTimeoutMS=maxSevSelDelay)
         else:    
             client = MongoClient("mongodb://localhost:27017/",serverSelectionTimeoutMS=maxSevSelDelay)        
         client.server_info()
@@ -111,16 +112,16 @@ def copy_file(src, dest):
         print("Path : {} tidak ditemukan!".format(dest))
 
 def is_reacheable(ip):            
-    if OS == 'posix' :
+    if OS == 'Linux' :
         if subprocess.call('ping -w ' + PING_INTERVAL + ' -c 1 ' + ip, shell=True, stdout=subprocess.PIPE) :
-            return 0
+            return False
         else:
-            return 1
+            return True
     else :
         if subprocess.call('ping -w ' + PING_INTERVAL*4000 + ' -n 1 ' + ip, shell=True, stdout=subprocess.PIPE) :
-            return 0
+            return False
         else:
-            return 1
+            return True
 
 def service_pinger(AC):
     global ROWS
@@ -152,7 +153,7 @@ def service_pinger(AC):
     
     PING_RESULT_IN = is_reacheable(VIPADDRIN)    
                 
-    if PING_RESULT_IN == 1 :
+    if PING_RESULT_IN :
         NINPINGOK+=1
         INDOOR = "1"
         DUPDATE = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
@@ -160,7 +161,7 @@ def service_pinger(AC):
             OUTDOOR = "1"
         else:
             PING_RESULT_OUT = is_reacheable(VIPADDROUT)
-            if PING_RESULT_OUT == 1 :
+            if PING_RESULT_OUT :
                 NOUTPINGOK+=1
                 OUTDOOR="1"
             else:
@@ -207,9 +208,10 @@ def main():
     print("===============================")
     print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"))
     print("===============================")
+    CONNECT_IOT = is_reacheable('172.24.24.1')      
     CONNECT_INFLUX = initialization_influx()
     CONNECT_MONGO = initialization_mongo()
-    if CONNECT_MONGO == True and CONNECT_INFLUX == True:
+    if CONNECT_IOT == True and CONNECT_MONGO == True and CONNECT_INFLUX == True:
         # QUERY_SELECT_DATA = { "VASSETID" : { "$regex": "^SIM" } }
         QUERY_SELECT_DATA = { "VASSETID" : { "$regex": "^ACO" } }
         # QUERY_SELECT_DATA = { "VASSETID" : { "$regex": "ACO.P1.A4" } }
@@ -233,6 +235,12 @@ def main():
         print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"))
         print("===============================")
     else :
+        if not CONNECT_IOT :
+            print("Application Stopped. because cannot connect IoT Network")
+        elif not CONNECT_MONGO:
+            print("Application Stopped. because cannot connect MongoDB")
+        elif not CONNECT_INFLUX:
+            print("Application Stopped. because cannot connect InfluxDB")
         sys.exit()    
 
 if __name__ == '__main__':
