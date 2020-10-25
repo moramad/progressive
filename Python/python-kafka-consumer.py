e45a4ffbf18d80da9dcd1394afc3f060daae05e5
mochamad <mochamadramadhan92@gmail.com>
2020-10-25 14:15:43 +0700

    re-commit

--- Python/python-kafka-consumer.py
+++ Python/python-kafka-consumer.py
@@ -5,7 +5,7 @@ consumer = KafkaConsumer(bootstrap_servers='localhost:9092',
                          auto_offset_reset='earliest',
                          value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 consumer.subscribe(['my-topic'])
-
+ 
 try :
     for message in consumer:    
         #get value from JSON
